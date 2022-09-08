31e43765a917df61448fc019d5b5e0da5c56be7a
C4rl0sA <106273776+C4rl0sA@users.noreply.github.com>
2022-09-08 09:46:27 -0500

    Update Pantalla.js

--- src/components/Pantalla.js
+++ src/components/Pantalla.js
@@ -1,12 +1,29 @@
 import React from 'react'
 
-export const Pantalla = ({Val}) => {
+
+export const Pantalla = ({ Val }) => {
+
+  const handleInputChange = (e) => {
+      e.defaultPrevent();
+  }
+
+  
+
+
+
   return (
     <>
         <h1>CALCULATOR</h1>
             <p>N y C</p>
             
-            <form><input type="text" defaultValue={Val}/></form>
+            <form>
+            <input 
+            type="text"
+            value={ Val }
+            onChange={ handleInputChange }
+            
+            />
+            </form>
     </>
   )
 }
